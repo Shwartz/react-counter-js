dc8a5edec48fec2aae71640314da0fc334ae4a59
andris.svarcs <andris.svarcs@yahoo.com>
2019-03-02 22:49:53 +0000

    Adding Counter and basic functionality

--- src/pages/lesson1/Lesson1.js
+++ src/pages/lesson1/Lesson1.js
@@ -1,14 +1,66 @@
 import React, {Component} from 'react';
+import {InputWidget} from './widget/InputWidget';
 
 export class Lesson1 extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <div>
-                <h1>Lesson1 page</h1>
-            </div>
-        )
-    }
+  state = {
+    inputValue: 5,
+    totalValue: 0
+  };
+
+  add() {
+    this.setState((currentState) => {
+      const {inputValue, totalValue} = currentState;
+      const result                   = inputValue + totalValue;
+      return {
+        inputValue,
+        totalValue: result
+      };
+    });
+  }
+
+  remove() {
+    this.setState((currentState) => {
+      const {inputValue, totalValue} = currentState;
+      const result                   = totalValue - inputValue;
+      return {
+        inputValue,
+        totalValue: result
+      };
+    });
+  }
+
+  totalValue(value) {
+    this.setState({
+                    inputValue: value
+                  });
+  }
+
+  render() {
+    const {inputValue, totalValue} = this.state;
+
+    return (
+      <div>
+        <h1>Lesson1 page</h1>
+        <div>
+          <p>Total: {totalValue}</p>
+
+          <InputWidget
+            add={() => {
+              this.add();
+            }}
+            remove={() => {
+              this.remove();
+            }}
+            inputValue={inputValue}
+          />
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Lesson1;
---
+++ src/pages/lesson1/widget/InputWidget.js
@@ -0,0 +1,32 @@
+import {Button, Input} from '@material-ui/core';
+import React, {Fragment} from 'react';
+import styles from './InputWidget.module.scss';
+
+export const InputWidget = ({change, add, remove, inputValue}) => {
+  return (
+    <Fragment>
+      <div className={styles.buttons}>
+        <Button
+          variant="contained"
+          onClick={() => {
+            remove();
+          }}>Remove {inputValue}
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() => {
+            add();
+          }}>Add {inputValue}
+        </Button>
+        <Input
+          type="number"
+          value={inputValue}
+          onChange={({currentTarget}) => {
+            change(+currentTarget.value);
+          }}
+          className={styles.inputValue}
+        />
+      </div>
+    </Fragment>
+  )
+};
