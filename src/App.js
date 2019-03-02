89bd882326e2d0644830c5e02912663ae64ccb32
Shvarcs <andris.svarcs@yahoo.com>
2019-03-02 11:49:22 +0000

    Adding Router, SCSS support, first two demo pages

--- src/App.js
+++ src/App.js
@@ -1,28 +1,62 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {CssBaseline, Grid, List, ListItem} from '@material-ui/core';
+import {HashRouter as Router, Link, Route, Switch} from 'react-router-dom';
+import styles from './App.module.scss';
+import React, {Component} from 'react';
+import Home from './pages/Home';
+import Lesson1 from './pages/lesson1/Lesson1';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = {
+        selectedIndex: 0
+    };
+
+    handleListItemClick = (ev, index) => {
+        this.setState({
+            selectedIndex: index
+        });
+    };
+
+    render() {
+        return (
+            <Router basename={process.env.PUBLIC_URL}>
+                <div className={styles.App}>
+                    <CssBaseline/>
+                    <Grid container={true} spacing={16}>
+                        <Grid item={true} sm={4} xs={12}>
+                            <List component="nav">
+                                <Link to="/">
+                                    <ListItem
+                                        button={true}
+                                        selected={this.state.selectedIndex === 1}
+                                        onClick={(ev) => this.handleListItemClick(ev, 1)}
+                                    >
+                                        Home
+                                    </ListItem>
+                                </Link>
+                                <Link to="/lesson-1">
+                                    <ListItem
+                                        button={true}
+                                        selected={this.state.selectedIndex === 2}
+                                        onClick={(ev) => this.handleListItemClick(ev, 2)}
+                                    >
+                                        Lesson 1 - Simple counter
+                                    </ListItem>
+                                </Link>
+                            </List>
+                        </Grid>
+                        <Grid item={true} sm={8} xs={12}>
+                            <section>
+                                <Switch>
+                                    <Route exact={true} path="/" component={Home}/>
+                                    <Route path="/lesson-1" component={Lesson1}/>
+                                </Switch>
+                            </section>
+                        </Grid>
+                    </Grid>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
