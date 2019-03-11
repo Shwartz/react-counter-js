ef08bdd3b2927248b1ad5a12643ff7a207599854
andris.svarcs <andris.svarcs@yahoo.com>
2019-03-11 18:07:11 +0000

    Adding config and prettier

--- src/pages/lesson1/Lesson1.js
+++ src/pages/lesson1/Lesson1.js
@@ -2,65 +2,65 @@ import React, {Component} from 'react';
 import {InputWidget} from './widget/InputWidget';
 
 export class Lesson1 extends Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  state = {
-    inputValue: 5,
-    totalValue: 0
-  };
+    state = {
+        inputValue: 5,
+        totalValue: 0
+    };
 
-  add() {
-    this.setState((currentState) => {
-      const {inputValue, totalValue} = currentState;
-      const result                   = inputValue + totalValue;
-      return {
-        inputValue,
-        totalValue: result
-      };
-    });
-  }
+    add() {
+        this.setState((currentState) => {
+            const {inputValue, totalValue} = currentState;
+            const result                   = inputValue + totalValue;
+            return {
+                inputValue,
+                totalValue: result
+            };
+        });
+    }
 
-  remove() {
-    this.setState((currentState) => {
-      const {inputValue, totalValue} = currentState;
-      const result                   = totalValue - inputValue;
-      return {
-        inputValue,
-        totalValue: result
-      };
-    });
-  }
+    remove() {
+        this.setState((currentState) => {
+            const {inputValue, totalValue} = currentState;
+            const result                   = totalValue - inputValue;
+            return {
+                inputValue,
+                totalValue: result
+            };
+        });
+    }
 
-  totalValue(value) {
-    this.setState({
-                    inputValue: value
-                  });
-  }
+    totalValue(value) {
+        this.setState({
+                          inputValue: value
+                      });
+    }
 
-  render() {
-    const {inputValue, totalValue} = this.state;
+    render() {
+        const {inputValue, totalValue} = this.state;
 
-    return (
-      <div>
-        <h1>Lesson1 page</h1>
-        <div>
-          <p>Total: {totalValue}</p>
+        return (
+            <div>
+                <h1>Lesson1 page</h1>
+                <div>
+                    <p>Total: {totalValue}</p>
 
-          <InputWidget
-            add={() => {
-              this.add();
-            }}
-            remove={() => {
-              this.remove();
-            }}
-            inputValue={inputValue}
-          />
-        </div>
-      </div>
-    )
-  }
+                    <InputWidget
+                        add={() => {
+                            this.add();
+                        }}
+                        remove={() => {
+                            this.remove();
+                        }}
+                        inputValue={inputValue}
+                    />
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Lesson1;
--- src/pages/lesson1/widget/InputWidget.js
+++ src/pages/lesson1/widget/InputWidget.js
@@ -3,30 +3,30 @@ import React, {Fragment} from 'react';
 import styles from './InputWidget.module.scss';
 
 export const InputWidget = ({change, add, remove, inputValue}) => {
-  return (
-    <Fragment>
-      <div className={styles.buttons}>
-        <Button
-          variant="contained"
-          onClick={() => {
-            remove();
-          }}>Remove {inputValue}
-        </Button>
-        <Button
-          variant="contained"
-          onClick={() => {
-            add();
-          }}>Add {inputValue}
-        </Button>
-        <Input
-          type="number"
-          value={inputValue}
-          onChange={({currentTarget}) => {
-            change(+currentTarget.value);
-          }}
-          className={styles.inputValue}
-        />
-      </div>
-    </Fragment>
-  )
+    return (
+        <Fragment>
+            <div className={styles.buttons}>
+                <Button
+                    variant="contained"
+                    onClick={() => {
+                        remove();
+                    }}>Remove {inputValue}
+                </Button>
+                <Button
+                    variant="contained"
+                    onClick={() => {
+                        add();
+                    }}>Add {inputValue}
+                </Button>
+                <Input
+                    type="number"
+                    value={inputValue}
+                    onChange={({currentTarget}) => {
+                        change(+currentTarget.value);
+                    }}
+                    className={styles.inputValue}
+                />
+            </div>
+        </Fragment>
+    )
 };
